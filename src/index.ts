eb973499a761646a27c47f2608e89a690ee38fb2
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2025-04-25 16:59:36 +0000

    Empezando con testing y con multithread

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import dotenv from 'dotenv';
+import cluster from 'cluster';
+import os from 'os';
+import app from './server'; // Tu aplicación Express
+
 dotenv.config();
 
-const port = process.env.PORT || 2030
+const numCPUs = os.cpus().length;
 
-import app from "./server"
+if (cluster.isPrimary) {
+    console.log(`Proceso primario ${process.pid} está corriendo`);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} murió. Código: ${code}, Señal: ${signal}`);
+        console.log('Iniciando un nuevo worker...');
+        cluster.fork();
+    });
+} else {
+    const port = process.env.PORT || 2030;
+    app.listen(port, () => {
+        console.log(`Worker ${process.pid} escuchando en http://localhost:${port}`);
+    });
+}
